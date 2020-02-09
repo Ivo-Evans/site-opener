fdbe8251f1c4f2c25f0186bd85d3a4501d164876
Ivo Evans <ievans.storrie@gmail.com>
2020-02-09 11:55:16 +0000

    add readme, format code

---
+++ readme.md
@@ -0,0 +1,34 @@
+This is a program to open a webpage in multiple browsers in a new desktop, so that you may check your site's cross-browser compatibility. You must have Ruby and wmctrl installed and, of course, you must be using Bash. 
+
+It works 'straight out of the box', but you should install it by adding it to your $PATH so that you can execute it from any location.
+
+Workspaces can be switched between with ctrl-alt-arrow
+
+## Adding sitelauncher.rb to your global path as an executable
+
+There are variations on how to do this, but here is how I did it:
+
+- Put sitelauncher.rb in 
+    ```home//<your username>/bin/``` e.g. ```home/ivo/bin/```
+  - If this directory doesn't exist (quite likely), make it
+- Set the address of your Ruby version on line 1 of sitelauncher.rb.
+  - Find this address with the terminal command ```which ruby```
+  - remember to include a ! after the # to make the comment on line 1 a shebang comment
+- make sitelauncher.rb an executable in terminal by running  
+  ```chmod +x sitelauncher.rb```
+  - make sure you're in the right folder
+- Make sure ```home//<your username>/bin/``` is in your $PATH
+  - see all the directories in your #PATH with  
+    ```sed 's/:/\n/g' <<< "$PATH"```
+  - if it isn't there (probable) add it to your .bashrc or .zshrc file (in ~)
+  - add this line to .bashrc and/or .zshrc  
+    ```export PATH=$PATH:"home/ivo/bin/" #obviously don't use ivo unless you are also called Ivo```
+- restart shell (you might need to log out and in again)
+- You should now be able to enter sitelauncher.rb from any location
+
+## using sitelauncher.rb
+
+- Just entering sitelauncher.rb should open the default site in a new desktop
+- edit the default if you like or override it by calling sitelauncher with a new URL, e.g.
+  ```sitelauncher.rb gogle.com```
+- add or remove browsers by adding or removing their relevant launch commands from the array ```browsers```
--- sitelauncher.rb
+++ sitelauncher.rb
@@ -1,4 +1,6 @@
 #! /usr/share/rvm/rubies/ruby-2.7.0/bin/ruby
+# frozen_string_literal: true
+
 default_address = 'http://127.0.0.1:5500/' # change this as convenient
 address = ARGV.empty? ? default_address : ARGV[0]
 
@@ -14,51 +16,12 @@ begin
   `wmctrl -s #{new_desktop}`
   # sleep 1 #re-enable if browsers sometimes open in old desktop not new one
   `#{browsers.map { |b| b + ' --new-window' + " \"#{address}\"" + ' & ' }.join('')}`
-  # `#{command}`
-rescue
-  puts 'The launcher encountered an error. It is likely that you need to install the dependency, wmctrl, e.g. on Ubuntu'
-  puts
-  puts "\tsudo apt-get install wmctrl"
-  puts
+rescue StandardError
+  puts "
+  The launcher encountered an error. It is likely that you need to install
+  wmctrl, e.g. on Ubuntu
+
+  \tsudo apt-get install wmctrl
+
+  "
 end
-
-# Instructions
-
-# 1. Running the program
-
-# run to open same website in multiple browsers in a single workspace
-# switch between workspaces with ctrl-alt-arrow
-# must have wmctrl installed
-# will open the default address by default, but if you include an address when you invoke the program it will use that instead. I have configured the default address to my normal localhost for vscode live server.
-
-
-
-# 2. 'installing' the program
-# This script can be used 'out of the packet' but if you add it to your $PATH as an executable you can use it from anywehre. There are variations on how to do this, but here is how I did it:
-
-# - Put sitelauncher.rb in home//<your username>/bin/ e.g. home/ivo/bin/
-#   - If this directory doesn't exist (quite likely), make it
-# - Set the address of your Ruby version on line 1 of sitelauncher.rb.
-#   - Find this address with the terminal command which ruby
-#   - remember to include a ! after the # to make the comment on line 1 a shebang comment
-# - make sitelauncher.rb an executable in terminal by running
-#   chmod +x sitelauncher.rb
-#   - make sure you're in the right folder
-# - Make sure home//<your username>/bin/ is in your $PATH
-#   - see all the directories in your #PATH with
-#     sed 's/:/\n/g' <<< "$PATH"
-#   - if it isn't there (probable) add it to your .bashrc or .zshrc file (in ~)
-#   - add this line to .bashrc and/or .zshrc
-#     export PATH=$PATH:"home/ivo/bin/" #obviously don't use ivo unless you are also called Ivo
-# - restart shell (you might need to log out and in again)
-# - You should now be able to enter sitelauncher.rb from any location
-
-# TODO: improve error handling
-
-
-
-
-
-
-
-
